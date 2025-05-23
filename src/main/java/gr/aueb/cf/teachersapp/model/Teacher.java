3844a7aaf9a362ab949c0a8385531dbfc749550b
Goatz <Goataragua@gmail.com>
2025-05-23 20:19:37 +0300

    add: new teacher entity

--- src/main/java/gr/aueb/cf/teachersapp/model/Teacher.java
+++ src/main/java/gr/aueb/cf/teachersapp/model/Teacher.java
@@ -1,15 +1,38 @@
 package gr.aueb.cf.teachersapp.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.UUID;
 
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
+@Table(name = "table")
 public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String uuid;
+
+    @Column(unique = true)
+    private String vat;
+    private String firstname;
+    private String lastname;
+
+    @PrePersist
+    public void initializeUUID() {
+        if (uuid == null) uuid = UUID.randomUUID().toString();
+    }
+
+    // private Region region;
+
 }
