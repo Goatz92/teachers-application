cc0b2951450fa987645ec36e9893e3b2f900a19b
Goatz <Goataragua@gmail.com>
2025-05-29 20:20:26 +0300

    feat: implement methods to class

--- src/main/java/gr/aueb/cf/teachersapp/repository/RegionService.java
+++ src/main/java/gr/aueb/cf/teachersapp/repository/RegionService.java
@@ -1,4 +1,26 @@
 package gr.aueb.cf.teachersapp.repository;
 
-public class RegionService {
+import gr.aueb.cf.teachersapp.model.static_data.Region;
+import gr.aueb.cf.teachersapp.service.IRegionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+//RequiredArgsConstructor
+public class RegionService implements IRegionService {
+
+    private final RegionRepository regionRepository;
+
+    @Autowired
+    public RegionService(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    @Override
+    public List<Region> findAllRegions() {
+        return regionRepository.findAll();
+    }
 }
