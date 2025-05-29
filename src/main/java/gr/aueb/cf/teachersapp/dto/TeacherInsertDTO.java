ce8f210f9090f3eef4a31a2da5a32f3d068825f1
Goatz <Goataragua@gmail.com>
2025-05-29 19:11:26 +0300

    feat: create insert teacher DTO

---
+++ src/main/java/gr/aueb/cf/teachersapp/dto/TeacherInsertDTO.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.teachersapp.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public class TeacherInsertDTO {
+
+    @NotNull(message = "Name can not be Null")
+    @Size(min = 2, message = "Name needs to have at least more than two letters")
+    private String firstname;
+
+    @NotNull(message= "Lastname can not be Null")
+    @Size(min = 2, message = "Lastname needs to have at least more than two letters")
+    private String lastname;
+
+    @Pattern(regexp = "\\d{9,}", message = "VAT can not be less than nine digits")
+    private String vat;
+    @NotNull(message = "Region can not be null")
+    private Long regionId;
+}
